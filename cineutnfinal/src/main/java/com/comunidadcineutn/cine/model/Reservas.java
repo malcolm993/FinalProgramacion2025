4d2f01e0fd275a45a7068b06bac97e3558fdab4d
santiago <santiagolopezzz.993@gmail.com>
2025-07-13 13:16:55 -0300

    modificacion al modelo de reservas se agrego la funcion es cancelable devuelve un boolean para verificar si se puede cancelar la funcion reservada.

--- cineutnfinal/src/main/java/com/comunidadcineutn/cine/model/Reservas.java
+++ cineutnfinal/src/main/java/com/comunidadcineutn/cine/model/Reservas.java
@@ -4,13 +4,59 @@
  */
 package com.comunidadcineutn.cine.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author santi
  */
+@Getter
+@Setter
+@Entity
+@Table(name = "reservas")
 public class Reservas {
-    
+
+    private final int CANTIDAD_DIAS_LIMITE = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne
+    @NotNull
     private Funcion funcionReservada;
+
+    @ManyToOne
+    @NotNull
+    private Usuario usuarioComprador;
+    private int costoReserva;
+    private int cantidadEntradas;
+
+    public Reservas() {
+    }
+
+    public Reservas(Funcion funcionReservada, Usuario usuarioComprador, int costoReserva, int cantidadEntradas) {
+
+        this.funcionReservada = funcionReservada;
+        this.usuarioComprador = usuarioComprador;
+        this.costoReserva = costoReserva;
+        this.cantidadEntradas = cantidadEntradas;
+    }
     
+    @Transient
+    public boolean isCancelable() {
+        return funcionReservada != null && 
+        LocalDate.now().isBefore(funcionReservada.getHoraInicio().toLocalDate().minusDays(CANTIDAD_DIAS_LIMITE));
+    }
+
 }
